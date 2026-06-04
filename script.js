f969dca5fbfe5678ed8944a2c8028bd57d7830d0
prashna2025 <prashnaraut32@gmail.com>
2026-06-04 11:45:44 +0545

    7th

--- script.js
+++ script.js
@@ -1,7 +1,84 @@
 const input = document.querySelector('input')
 const btn = document.querySelector('button')
 
-const dictionary = document.querySelector("dictionary-app") 
+const dictionary = document.querySelector(".dictionary-app")
+
+
+// https://api.dictionaryapi.dev/api/v2/entries/en/<word>
+
+async function dictionaryFn(word) {
+    const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+        .then(res => res.json())
+
+    return res [0]
+
+}
+
+
+btn.addEventListener('click', fetchandCreateCard)
+
+async function fetchandCreateCard() {
+    const data = await dictionaryFn(input.value)
+
+
+    console.log(data)
+
+
+    let partOfSpeechArray = []
+
+    for(let i=0 ; i <data.meanings.length-1 ; i++){
+        partsOfSpeechArray.push(data,meanings[i].partOfSpeech)
+    }
+    
+    
+
+    dictionary.innerHTML = `
+     <div class="card">
+
+                <div class="property">
+                    <span>Word</span>
+                    <span>${data.word} </span>
+
+
+                </div>
+
+
+                <div class="property">
+                    <span>Phonetics</span>
+                    <span>${data.phonetics} </span>
+
+                </div>
+
+
+                <div class="property">
+                    <span>
+                    <audio controls src="${data.phonetics[0].audio}"></audio>
+                    </span>
+                </div>
+
+
+                <div class="property">
+                    <span>Defination</span>
+                    <span>${data.meanings[0].definationss[0].definition}</span>
+
+                </div>
+
+
+                <div class="property">
+                    <span>Example</span>
+                    <span>${data.meanings[1].definationss[0].example}</span>
+
+                </div>
+
+
+                <div class="property">
+                    <span> ${PartOfSpeechArray.map(e => e).join (',')}</span>
+                </div>
+
+            </div> 
+
+    `
+}
 
 
 
